4fdbd210649c59600c5f6b439f3db41c14710144
IsmaelRvra130 <47541610+IsmaelRvra130@users.noreply.github.com>
2020-04-05 23:21:00 -0400

    Add files via upload
    NavBar and Logo rendering

---
+++ Home.js
@@ -0,0 +1,19 @@
+import React, { components } from "react";
+import Navbar from "../components/Navbar";
+import ElevateAppBar from "../components/Navbar";
+
+
+
+
+
+export default function Home (props) {
+   
+        return(
+            <Navbar>
+                <ElevateAppBar />
+            </Navbar>
+        
+        )
+     
+}
+
--- index.js
+++ index.js
@@ -1,12 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Logo from './logo.png';
+import "./style.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function ElevateAppBar(props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <ElevationScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <Typography style={{backgroundColor: "black"}} variant="h6"> <img src={Logo} alt= "src/components/Navbar/logo.png" />  </Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
+      <Container>
+        {/* <Box my={2}>
+          {[...new Array(12)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
+        </Box> */}
+      </Container>
+    </React.Fragment>
+  );
+}
